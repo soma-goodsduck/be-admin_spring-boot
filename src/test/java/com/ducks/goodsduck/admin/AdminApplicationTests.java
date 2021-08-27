ca7f6e44427f529c66f4fa3247c7556ec6729130
DESKTOP-4A0HAH2\genius <kwpark96@gmail.com>
2021-08-27 13:34:05 +0900

    [chore] 테스트 코드 삭제

--- src/test/java/com/ducks/goodsduck/admin/AdminApplicationTests.java
+++ src/test/java/com/ducks/goodsduck/admin/AdminApplicationTests.java
@@ -3,7 +3,6 @@ package com.ducks.goodsduck.admin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AdminApplicationTests {
 
 	@Test

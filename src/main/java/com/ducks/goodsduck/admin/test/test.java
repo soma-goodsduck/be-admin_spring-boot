560002828d78c90a98b5e0235a0f3325688b58c8
DESKTOP-4A0HAH2\genius <kwpark96@gmail.com>
2021-08-27 23:40:57 +0900

    [chore] re deploy

--- src/main/java/com/ducks/goodsduck/admin/test/test.java
+++ src/main/java/com/ducks/goodsduck/admin/test/test.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class test {
     @GetMapping("/test")
     public String test(){
-        String testStr = "Hi~~";
+        String testStr = "Hi!";
         System.out.println(testStr);
         return testStr;
     }

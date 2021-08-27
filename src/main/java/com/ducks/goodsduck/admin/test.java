d37348fe73e21c4d6a151776812f574abf012559
DESKTOP-4A0HAH2\genius <kwpark96@gmail.com>
2021-08-27 20:39:06 +0900

    [feat] add deploy

---
+++ src/main/java/com/ducks/goodsduck/admin/test.java
@@ -0,0 +1,14 @@
+package com.ducks.goodsduck.admin;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+    @GetMapping("/test")
+    public String test(){
+        String testStr = "Hi~~";
+        System.out.println(testStr);
+        return testStr;
+    }
+}

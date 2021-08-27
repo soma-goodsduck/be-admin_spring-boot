63e8d499f0694cba95dce3dee30cbb4e3aa4a0b2
DESKTOP-4A0HAH2\genius <kwpark96@gmail.com>
2021-08-28 00:48:59 +0900

    [chore] check

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,11 @@ JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z $CURRENT_PID ]
+if [ -z "$CURRENT_PID" ]
 then
   echo "> 종료할것 없음."
 else
-  echo "> kill -9 $CURRENT_PID"
+  echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi

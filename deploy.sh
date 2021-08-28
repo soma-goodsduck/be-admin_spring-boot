7814e3fcc55e03c398035d7735e2fce887a2f1da
DESKTOP-4A0HAH2\genius <kwpark96@gmail.com>
2021-08-28 11:37:35 +0900

    [chore] check

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,40 @@
-#!/usr/bin/env bash
+##!/usr/bin/env bash
+#
+#REPOSITORY=/opt/be-admin_spring-boot
+#cd $REPOSITORY
+#
+#APP_NAME=be-admin_spring-boot
+#JAR_PATH=$REPOSITORY/build/libs/
+#
+#CURRENT_PID=$(pgrep -f $APP_NAME)
+#
+#echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
+#
+#if [ -z "$CURRENT_PID" ]
+#then
+#  echo "> 종료할것 없음."
+#else
+#  echo "> kill -15 $CURRENT_PID"
+#  kill -15 $CURRENT_PID
+#  sleep 5
+#fi
+#
+#echo "> $JAR_PATH 배포"
+#
+#JAR_NAME=$(ls $JAR_PATH | grep '.jar' | tail -n 1)
+#
+##nohup java -jar $JAR_NAME > $JAR_PATH/nohup.out 2>&1 &
+#
+#nohup java -jar $JAR_NAME > /dev/null 2> /dev/null < /dev/null &
 
 REPOSITORY=/opt/be-admin_spring-boot
 cd $REPOSITORY
 
 APP_NAME=be-admin_spring-boot
-JAR_PATH=$REPOSITORY/build/libs/
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
-
-echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]
 then
@@ -21,8 +47,5 @@ fi
 
 echo "> $JAR_PATH 배포"
 
-JAR_NAME=$(ls $JAR_PATH | grep '.jar' | tail -n 1)
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
 
-#nohup java -jar $JAR_NAME > $JAR_PATH/nohup.out 2>&1 &
-
-nohup java -jar $JAR_NAME > /dev/null 2> /dev/null < /dev/null &

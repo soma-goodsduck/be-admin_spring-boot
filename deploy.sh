0b25f51adcd4109718a5613d05a0c8e5ba38069a
DESKTOP-4A0HAH2\genius <kwpark96@gmail.com>
2021-08-28 11:43:12 +0900

    [chore] check

--- deploy.sh
+++ deploy.sh
@@ -31,10 +31,10 @@ REPOSITORY=/opt/be-admin_spring-boot
 cd $REPOSITORY
 
 APP_NAME=be-admin_spring-boot
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
+
+echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]
 then
@@ -47,5 +47,10 @@ fi
 
 echo "> $JAR_PATH 배포"
 
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+#nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+
+nohup java -jar $JAR_PATH > $REPOSITORY/nohup.out 2>&1 &
 

20fe3ab36a6227a769d426769acd10dea2eb44e4
DESKTOP-4A0HAH2\genius <kwpark96@gmail.com>
2021-09-29 18:46:59 +0900

    [chore] check

--- src/main/java/com/ducks/goodsduck/admin/AdminApplication.java
+++ src/main/java/com/ducks/goodsduck/admin/AdminApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:/application-s3.yml")
 public class AdminApplication {
 
 	public static void main(String[] args) {
